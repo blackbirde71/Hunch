021c5b992677b222697649ab3045b2f51f7ba636
NathanVosburg <nathanjvosburg@gmail.com>
2025-10-04 15:58:05 -0400

    added getUnansweredQuestions

--- frontend/hunch/lib/database.dart
+++ frontend/hunch/lib/database.dart
@@ -64,6 +64,42 @@ Future<List<Map<String, dynamic>>> getQuestionsByIds(List<int> ids) async {
   }).toList();
 }
 
+Map<String, dynamic> _processQuestion(Map<String, dynamic> question) {
+  if (question['picture_base64'] != null) {
+    question['picture_data'] =
+        base64Decode(question['picture_base64'] as String);
+  }
+  return question;
+}
+
+Future<List<Map<String, dynamic>>> getUnansweredQuestions(
+    int limit, List<int> inCache) async {
+  if (limit == 0) return [];
+
+  final user = Supabase.instance.client.auth.currentUser;
+  if (user == null) {
+    // If not signed in, just return random questions
+    final questions = await supabase.from('questions').select().limit(limit);
+    return questions.map(_processQuestion).toList();
+  }
+
+  final answered = await supabase
+      .from('user_interactions')
+      .select('question_id')
+      .eq('user_id', user.id);
+
+  final answeredIds = answered.map<int>((r) => r['question_id'] as int).toList()
+    ..addAll(inCache);
+
+  var query = supabase.from('questions').select();
+  if (answeredIds.isNotEmpty) {
+    query = query.not('id', 'in', answeredIds);
+  }
+
+  final questions = await query.limit(limit);
+  return questions.map(_processQuestion).toList();
+}
+
 Future<void> sendAnswers(List<Answer> answers) async {
   if (answers.isEmpty) return;
 
